607b0b4f4213bc56d80daf1d0726fa1dc20e9873
Ag0r9 <51130332+Ag0r9@users.noreply.github.com>
2022-08-15 12:17:58 +0200

    fix: notebooks rerun with fixed get_metrics

--- classic_CV.py
+++ classic_CV.py
@@ -13,9 +13,9 @@ def create_result_classic(image, mask):
     mask = (frangi(mask))
     mask = cv2.blur(mask, (30, 30))
     mask = normalize(mask)
-    _, mask = cv2.threshold(mask, 190, 255, cv2.THRESH_BINARY)
+    _, mask = cv2.threshold(mask, 250, 255, cv2.THRESH_BINARY)
     image = normalize(filtered)
-    _, image = cv2.threshold(image, 230, 255, cv2.THRESH_BINARY_INV)
+    _, image = cv2.threshold(image, 250, 255, cv2.THRESH_BINARY_INV)
     image, mask = image.astype(np.uint8), mask.astype(np.uint8)
     image = cv2.bitwise_and(image, image, mask=mask)
     return image
